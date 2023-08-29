e433363fa118cd5e1a71e2f25781b85c222ec11b
QuinnFeng <quinnfeng26276@gmail.com>
2023-08-29 17:19:43 -0400

    completed assignment

--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
+
 export const FunctionalTrafficLight = () => {
+
+  const [index, setIndex] = useState(0);
+
+  const updateIndex = () => {
+    setIndex((index + 1) % 3);
+  }
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
         {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div className={`circle ${index == 0 ? 'red' : 'black'}`}></div>
+        <div className={`circle ${index == 1 ? 'yellow' : 'black'}`}></div>
+        <div className={`circle ${index == 2 ? 'green' : 'black'}`}></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className="next-state-button" onClick={updateIndex}>Next State</button>
     </div>
   );
 };
